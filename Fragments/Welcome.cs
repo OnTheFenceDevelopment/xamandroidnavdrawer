fdc55f5f4294c03df1fa181976966c3f9251bdbb
Dave Carson <david.carson@agiledatum.com>
2020-02-17 08:40:23 +0000

    Working Solution

---
+++ Fragments/Welcome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Support.V4.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace NavigationDrawerStarter.Fragments
+{
+    public class Welcome : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.welcome, container, false);
+        }
+    }
+}
